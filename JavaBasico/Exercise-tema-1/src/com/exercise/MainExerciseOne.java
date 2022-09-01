ca7ae7284537e57a38825f2ed24ac1aa977f5468
unknown <Lautaro Ferreyra>
2022-09-01 10:10:11 -0300

    exercise 1 the java

---
+++ JavaBasico/Exercise-tema-1/src/com/exercise/MainExerciseOne.java
@@ -0,0 +1,12 @@
+package com.exercise;
+
+public class MainExerciseOne {
+    public static void main(String[] args) {
+        int number = 888888888;
+        long numberLong = 351334448;
+        double numberDouble = 10.6;
+        String string = "hi i´m a string";
+
+        System.out.println("int: " + number + "; long: "+ numberLong + "; double: " + numberDouble+ "; string: " + string);
+    }
+}
